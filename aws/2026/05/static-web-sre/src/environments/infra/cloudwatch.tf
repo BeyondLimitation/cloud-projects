e49cb9d42eecdef43592901d4cde26d46c580954
lee heechan <leeheechan1@gmail.com>
2026-08-04 17:21:56 +0900

    2026-08-04 변경 사항
    2026-05월 static-web-sre
    Terraform: SNS Topic Subscription 추가.
    Github: Terraform의 환경 변수를 Github Action의 Secret 값으로 대체

--- aws/2026/05/static-web-sre/src/environments/infra/cloudwatch.tf
+++ aws/2026/05/static-web-sre/src/environments/infra/cloudwatch.tf
@@ -1,3 +1,9 @@
 resource "aws_sns_topic" "error-budget" {
   name = "static-web-sre-5XX_error_budget"
 }
+
+resource "aws_sns_topic_subscription" "via_email" {
+  topic_arn = aws_sns_topic.error-budget.arn
+  protocol = "email"
+  endpoint = var.email_address
+}
