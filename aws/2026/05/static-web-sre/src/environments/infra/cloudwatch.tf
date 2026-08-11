4f8652d29388ad97abdd1ebe4b9b70c467360f79
lee heechan <leeheechan1@gmail.com>
2026-08-11 20:40:58 +0900

    2026-08-11 변경 사항
    2026-05월 static-web-sre
    Terraform: 리소스 의존성 명시

--- aws/2026/05/static-web-sre/src/environments/infra/cloudwatch.tf
+++ aws/2026/05/static-web-sre/src/environments/infra/cloudwatch.tf
@@ -8,6 +8,7 @@ resource "aws_sns_topic_subscription" "via_email" {
   topic_arn = aws_sns_topic.error-budget.arn
   protocol  = "email"
   endpoint  = var.email_address
+  depends_on = [ aws_sns_topic.error-budget ]
 }
 
 # CloudWatch Alarm 설정
