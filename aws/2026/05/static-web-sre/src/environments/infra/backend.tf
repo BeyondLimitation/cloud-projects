617df20aaabb0c407abef89abb928c2120261f4f
lee heechan <leeheechan1@gmail.com>
2026-08-11 19:27:06 +0900

    2026-08-11 변경 사항
    2026-05월 static-web-sre
    Terraform: 'backend.tf' 에 provider 를 설정해 기본 작업 리전을 설정

--- aws/2026/05/static-web-sre/src/environments/infra/backend.tf
+++ aws/2026/05/static-web-sre/src/environments/infra/backend.tf
@@ -2,5 +2,10 @@ terraform {
   backend "s3" {
     bucket = "lee-static-web-sre-state-storage"
     key    = "infra/state.tfstate"
+    region = "ap-northeast-2"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
