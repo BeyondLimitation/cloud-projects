61525c94883ec63bf6af2948410ac5cb71839047
lee heechan <leeheechan1@gmail.com>
2026-08-18 19:00:47 +0900

    2026-08-18 변경 사항
    2026-05월 static-web-sre
    
    Terraform: 'outputs.tf'에 CloudFront 도메인 주소 출력이 되도록 설정
    Github: Workflow를 재정의. 연결 테스트 준비

--- aws/2026/05/static-web-sre/src/environments/infra/outputs.tf
+++ aws/2026/05/static-web-sre/src/environments/infra/outputs.tf
@@ -3,6 +3,11 @@ output "cloudfront_distribution_id" {
   value       = aws_cloudfront_distribution.static-website-sre.id
 }
 
+output "cloudfront_domain_name" {
+  description = "CloudFront 도메인"
+  value       = aws_cloudfront_distribution.static-website-sre.domain_name
+}
+
 output "aws_sns_topic_arn" {
   description = "SNS Topic의 ARN"
   value       = aws_sns_topic.error-budget.arn
